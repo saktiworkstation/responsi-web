b4d790b4ecc0a837bc00b8896b11dbe82ae12690
saktiworkstation <saktiworkstation@gmail.com>
2024-06-28 22:38:59 +0700

    nux config

--- .nuxt/dist/server/client.manifest.mjs
+++ .nuxt/dist/server/client.manifest.mjs
@@ -1,1 +1,1 @@
-export { default } from "file:///Users/mujiyanto/Documents/server/frontend/pw/my-nuxt-portfolio/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
+export { default } from "file:///E:/Kuliah%20Semester%204/PemrogWEB/responsi-web/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
